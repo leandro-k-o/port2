4a12c27a450f3fddbc3f0eaeb39141449857c346
leandro <leandro.k.oshiro@gmail.com>
2022-07-28 13:49:36 -0300

    ajustando a classe projetos

--- assets/script.js
+++ assets/script.js
@@ -78,16 +78,181 @@ class RotateBall extends Scroll{
         this.ball.classList.add('rotate-ball');
         let ballTop = (this.ball.getBoundingClientRect().top * 1.5);
         if(ballTop > 0){
-            document.documentElement.style.setProperty('--degree',`${ballTop}deg`);
+            this.ball.style.setProperty('--degree',`${ballTop}deg`);
         }
         
     }
 
 }
 
-new ScrollIn('.scroll-in');
-new RotateBall('#balls');
+class Projetos extends Scroll{
 
-window.addEventListener('load', (e) => {
-    window.scrollTo(0,0);;
-  });
+    
+
+    super(name){}
+
+    init(){
+            this.h2 = document.querySelector('#projetos h2');
+            this.h2_before = window.getComputedStyle(this.h2,'::before');
+
+            this.projetos_container_wrapper = document.querySelector('.projetos-container-wraper');
+            this.projetos_container_wrapper.classList.add('scroll-in');
+
+            this.projetos_container = this.elements[0];
+
+            this.li = document.querySelectorAll('ul.projetos-menu-lista li');
+
+            this.verElementos();
+
+            this.elements.forEach((element)=>{
+                this.observer.observe(element)
+            })
+    }
+
+    verElementos(){
+        this.options = {
+            threshold: .6,
+            rootMargin: '',
+        };
+        this.observer = new IntersectionObserver((entries, observer)=>{
+            entries.forEach((entry)=>{
+                this.criarLinhas(entry);
+            })
+        },this.options)
+    }
+
+    criarLinhas(entry){
+        if(entry.isIntersecting){
+            this.h2.style.setProperty('--widthH2B','0px');
+            this.projetos_container.style.setProperty('--heightPCB','0%');
+
+            this.li.forEach((el)=>{
+                el.style.setProperty('--widthLi','0px');
+                el.style.setProperty('opacity','0');
+            })
+
+            this.desenharLinhas();
+
+            this.projetos_container_wrapper.classList.add('scrolled');
+            
+            this.observer.unobserve(entry.target);
+
+        }else{
+            this.h2.style.setProperty('--widthH2B','0px');
+            this.projetos_container.style.setProperty('--heightPCB','0%');
+            this.projetos_container.style.setProperty('--heightPCB','0%');
+            this.li.forEach((el)=>{
+                el.style.setProperty('--widthLi','0px');
+                el.style.setProperty('opacity','0');
+            })
+        }
+    }
+
+    desenharLinhas(){
+        this.h2.style.setProperty('--widthH2B','200px');
+        this.projPromisse = new Promise((resolve)=>
+            window.setTimeout(()=> {
+                this.projetos_container.style.setProperty('--heightPCB','100%')
+                resolve()
+            },500)
+        )
+        .then(()=>this.li.forEach((el)=>{
+            el.style.setProperty('--widthLi','28px');
+            el.style.setProperty('opacity','1');
+        }))     
+    }  
+
+    projPromisse(){
+        return this.projPromisse;
+    }
+        
+}
+
+class ProjetosSlider {
+
+    constructor(){
+        this.setAllItems();
+        this.init();
+    }
+    
+    init(){
+        this.projetoItem.forEach((item,i)=>{
+            this.itemMedidas.push({
+                'item': item, 
+                'offsetleft': item.offsetLeft,
+                'medida': this.calcularMedida(item)
+            });
+
+            this.projetosMenu[i].addEventListener('mouseover',(e) => {
+                this.selecionarItem(this.itemMedidas[i]);
+            })
+
+            this.projetosMenu[i].addEventListener('mouseout',(e) => {   
+                this.selecionarMenu(e, i);    
+                //this.slider.classList.remove('projetos-slider-active');
+            })
+
+            this.projetosMenu[i].addEventListener('click',(e) => {
+                e.preventDefault();
+                this.selecionarItem(this.itemMedidas[i]);
+                this.selecionarMenu(e, i);
+            })
+        })
+        if(this.lastActive === null)
+            this.selecionarItem(this.itemMedidas[0]);
+
+        console.log(this)
+    }
+
+    setAllItems(){
+        this.slider = document.querySelector('.projetos-slider');
+        this.projetoItem = document.querySelectorAll('.projeto-item');
+        this.wraper = document.querySelector('.projetos-container-wraper');
+        this.projetosMenu = document.querySelectorAll('.projetos-menu-lista li a');
+        this.itemMedidas = [];
+        this.lastActive = null;
+    }
+
+    refresh(){
+        this.setAllItems()
+        this.init();
+    }
+
+    calcularMedida(item){
+        const margin = (this.wraper.offsetWidth - item.offsetWidth) / 2;
+        return -(item.offsetLeft - margin)
+    }
+
+    selecionarItem({medida}){
+        this.slider.style.setProperty('--item',`${medida}px`);
+        this.slider.classList.add('projetos-slider-active');
+    }
+
+    selecionarMenu(event, i){
+        event.currentTarget.children[0].classList.add('produto-active');               
+        if(this.lastActive!==null && this.lastActive !== i) 
+            this.projetosMenu[this.lastActive].children[0].classList.remove('produto-active');
+        this.lastActive = i;
+    }
+}
+
+let projetoLoad;
+let projeto;
+window.addEventListener("load",() => {
+    new ScrollIn('.scroll-in');
+    new RotateBall('#balls');
+    if(window.innerWidth > 1050){
+        projeto = new Projetos('.projetos-container');
+    }
+    let p = projeto.projPromisse()
+    console.log(p) 
+    projetoLoad = new ProjetosSlider();
+    window.scrollTo(0,0);
+});
+
+window.onresize = () => 
+{
+    projetoLoad.refresh();
+}
+
+
